fec8105d2985c9d6ef7238953f18e2ae42415344
Philip Garus <philipgarus97@gmail.com>
2021-12-08 14:52:10 +0100

    Added Input Field , finished Part 2

---
+++ src/components/InputTodo.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react"
+
+// class type since it will hold state => the user input; or use React Hooks
+class InputTodo extends Component{
+
+    state={
+        title:"",
+    };
+
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        if(this.state.title.trim()){
+            this.props.addTodoProps(this.state.title);
+            this.setState({
+                title: ""
+            })
+        } else {
+            alert("Please write item")
+        }
+
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                       placeholder="Add Todo..."
+                       value={this.state.title}
+                       name="title"
+                       onChange={this.onChange}
+                />
+                <button>Submit</button>
+            </form>
+        )
+    }
+}
+export default InputTodo
