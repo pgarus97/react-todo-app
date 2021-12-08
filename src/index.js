60ad53cc83559a1c29aea8016af628eb3278f459
Philip Garus <philipgarus97@gmail.com>
2021-12-08 16:04:09 +0100

    Part 3.1 added CSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import "./App.css"
 import reportWebVitals from './reportWebVitals';
 import TodoContainer from "./components/TodoContainer";
 
